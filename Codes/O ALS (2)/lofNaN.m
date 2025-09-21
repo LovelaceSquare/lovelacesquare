58be10cd947cf14170f8d304ab25324b885e52d8
Adrián <gomez.sanchez.adr@gmail.com>
2025-09-21 18:32:31 +0200

    Remove Codes content

--- Codes/O ALS (2)/lofNaN.m
+++
@@ -1,8 +0,0 @@
-function [r2,lofc]=lofNaN(de,c,s)
-dr=c*s;
-res=de-dr;
-de=de(:); res=res(:);
-iN=find(~isnan(res));
-lofc=100*sqrt((res(iN)'*res(iN))/(de(iN)'*de(iN)));
-r2=(1-(lofc/100)^2)*100;
-end
